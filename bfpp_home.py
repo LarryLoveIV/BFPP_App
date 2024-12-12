148779aa49dab458aa836824cbe953513a280c68
Larry Love <lawrenceloveiv@gmail.com>
2024-12-12 08:05:24 -0500

    Initial commit

---
+++ bfpp_home.py
@@ -0,0 +1,109 @@
+import streamlit as st
+import functions as func
+
+st.markdown("""## <b><u>2024 Burholme Fantasy Playoff Pool</u></b>""", unsafe_allow_html=True)
+st.markdown("""**One entry per person - $20 Venmo @Larry-LoveIV. Build one team to ride throughout the playoffs and get the highest cumulative score through the Super Bowl.**
+
+**Top 3 cumulative scores will earn prizes. Last season we had 29 entries for a total pot of $580. Let's try to get a bigger pot this year - if we do, the prizes will look like this:**
+
+**1st - Remaining pot \(\$460 if we reach 30 entries\)  \n2nd - \$100  \n3rd - \$40**
+
+**Submissions must be in before Browns @ Texans Kickoff at 4:30PM on 1/13.**""")
+st.markdown("""#### <b><u>Team Building Rules:</u></b>  
+Fill the positions below using players from the all the playoff teams - you can use no more than 2 players from the 
+same team. i.e: you can take Goff and St. Brown, but you can't have Goff, St. Brown, and Gibbs/Lions DST/Jake Bates.
+
+Your first flex position will get you 1.5x points. Last year we had a 2x points flex position, 
+but I think 1.5x makes it less important to get that position correct and hopefully diversifies the player ownership there.""", unsafe_allow_html=True)
+
+#
+st.markdown("""#### <b><u>Positions:</u></b>  
+2 QBs  
+4 RBs  
+4 WRs  
+2 TEs  
+1 Flex (RB,WR, or TE) - 1.5x Points!  
+1 Flex (RB,WR, or TE)  
+1 Def/Special Teams  
+1 Kicker
+""", unsafe_allow_html=True)
+
+# st.markdown("""<b><u>Scoring:</u></b>
+# **Passing**
+# * Touchdown = 6
+# * Yards = 1 / 30 Yards
+# * 2 Pt Conv. = 2
+# * Interception = -2
+# * Fumble = -2
+#
+# **Receiving**
+# * Touchdown = 6
+# * Yards = 1 / 10 Yards
+# * 2 Pt Conv. = 2
+# * Reception = 1
+# * Reach 100 Yards = 3
+# * Fumble = -2
+#
+# **Rushing**
+# * Touchdown = 6
+# * Yards = 1 / 10 Yards
+# * 2 Pt Conv. = 2
+# * Reach 100 Yards = 3
+# * Fumble = -2
+#
+# **Kickers - No yardage bonus**
+# * FG = 3
+# * XP = 1
+#
+# **Defense/ST**
+# * 0 Points allowed = 12
+# * 2-10 Points allowed = 8
+# * 11-20 Points allowed = 5
+# * 21-30 Points allowed = 0
+# * 31+ Points allowed  = -5
+# * Takeaways = 2
+# * Touchdowns = 6
+# """, unsafe_allow_html=True)
+st.markdown("""#### <b><u>Scoring - No decimals:</u></b>""", unsafe_allow_html=True)
+
+col1, col2, col3= st.columns([1,1,1])
+col5, col6, col7= st.columns([1,1,1])
+
+with col1:
+    st.markdown("""**Passing**
+* Touchdown = 6
+* Yards = 1 / 30 Yards
+* 2 Pt Conv. = 2
+* Interception = -2
+* Fumble = -2""", unsafe_allow_html=True)
+with col2:
+    st.markdown("""**Receiving**
+* Touchdown = 6
+* Yards = 1 / 10 Yards
+* 2 Pt Conv. = 2
+* Reception = 1
+* Reach 100 Yards = 3
+* Fumble = -2""", unsafe_allow_html=True)
+with col3:
+    st.markdown("""**Rushing**
+* Touchdown = 6
+* Yards = 1 / 10 Yards
+* 2 Pt Conv. = 2
+* Reach 100 Yards = 3
+* Fumble = -2""", unsafe_allow_html=True)
+with col5:
+    st.markdown("""**Kickers - No yardage bonus**
+* FG = 3
+* XP = 1""", unsafe_allow_html=True)
+with col6:
+    st.markdown("""**Defense/ST**
+* 0 Points allowed = 12
+* 2-10 Points allowed = 8
+* 11-20 Points allowed = 5
+* 21-30 Points allowed = 0
+* 31+ Points allowed  = -5
+* Takeaways = 2
+* Touchdowns = 6""", unsafe_allow_html=True)
+
+if __name__ == '__main__':
+    func.user_input()
---
+++ functions.py
@@ -0,0 +1,103 @@
+import streamlit as st
+import pandas as pd
+import gspread as gs
+
+gc = gs.service_account_from_dict(st.secrets.cred)
+sh1 = gc.open('2024 Playoff Fantasy Football Player Pool')
+ws1 = sh1.worksheet('2024 Player Pool')
+
+df = pd.DataFrame.from_dict(ws1.get_all_records())
+
+        #
+        # if st.button("Submit"):
+        #     # # Save user selections to Google Sheets
+        #     # client = authenticate_google_sheets()
+        #     # sheet = client.open('Fantasy Football Draft Selections').sheet1
+        #
+        #     # Prepare data to be saved
+        #     data = {
+        #         'Name': user_name,
+        #         'QB': qb_selection,
+        #         'RB1': rb_selections[0] if len(rb_selections) > 0 else '',
+        #         'RB2': rb_selections[1] if len(rb_selections) > 1 else '',
+        #         'WR1': wr_selections[0] if len(wr_selections) > 0 else '',
+        #         'WR2': wr_selections[1] if len(wr_selections) > 1 else '',
+        #         'TE': te_selection,
+        #         'Flex': section_5_selection,
+        #     }
+        #
+        #     # # Append to Google Sheet
+        #     # sheet.append_row([data[key] for key in data])
+        #
+        #     st.success(f"Thank you, {user_name}! Your selections have been submitted.")
+
+# Create a dictionary for holding player selections
+selected_players = []
+
+# 3. Define a function to collect user input
+def user_input():
+    # Ask for user name
+    user_name = st.text_input("Enter your name:")
+
+    if user_name:
+
+        # Create user input form for player selections
+        with st.form(key='team_selection'):
+            # QB Selection
+            qb_choices = df[df['Position'] == 'QB']
+            qb = st.multiselect('Select 1 QB:', qb_choices['Player'].tolist(), max_selections=1)
+            selected_players = [{'Player': player, 'Position': 'QB',
+                                 'Team': qb_choices[qb_choices['Player'] == player]['Team'].values[0]} for player in qb]
+
+            # RB Selections (limit to 2 players)
+            rb_choices = df[df['Position'] == 'RB']
+            rbs = st.multiselect('Select 2 RBs:', rb_choices['Player'].tolist(), max_selections=2)
+            selected_players += [{'Player': player, 'Position': 'RB',
+                                  'Team': rb_choices[rb_choices['Player'] == player]['Team'].values[0]} for player in
+                                 rbs]
+
+            # WR Selections (limit to 2 players)
+            wr_choices = df[df['Position'] == 'WR']
+            wrs = st.multiselect('Select 2 WRs:', wr_choices['Player'].tolist(), max_selections=2)
+            selected_players += [{'Player': player, 'Position': 'WR',
+                                  'Team': wr_choices[wr_choices['Player'] == player]['Team'].values[0]} for player in
+                                 wrs]
+
+            # TE Selection
+            te_choices = df[df['Position'] == 'TE']
+            te = st.multiselect('Select 1 TE:', te_choices['Player'].tolist(), max_selections=1)
+            selected_players += [{'Player': player, 'Position': 'TE',
+                                  'Team': te_choices[te_choices['Player'] == player]['Team'].values[0]} for player in
+                                 te]
+
+            # Flexible Position Selection (RB/WR/TE)
+            flex_choices = df[df['Position'].isin(['RB', 'WR', 'TE'])]
+            flex = st.multiselect('Select 1 additional player from RB/WR/TE:', flex_choices['Player'].tolist(),
+                                  max_selections=1)
+            selected_players += [
+                {'Player': player, 'Position': flex_choices[flex_choices['Player'] == player]['Position'].values[0],
+                 'Team': flex_choices[flex_choices['Player'] == player]['Team'].values[0]} for player in flex]
+
+            # Submit button
+            submit_button = st.form_submit_button("Submit Selection")
+
+            # Handle the submission logic
+            if submit_button:
+                # Count how many players have been selected per team
+                team_counts = {}
+                for selection in selected_players:
+                    team = selection['Team']
+                    team_counts[team] = team_counts.get(team, 0) + 1
+
+                # Check for any teams with more than 2 players
+                error_teams = [team for team, count in team_counts.items() if count > 2]
+
+                if error_teams:
+                    st.error(f"Error: You have selected more than 2 players from the following teams: {', '.join(error_teams)}. Please fix your selection.")
+                else:
+                    st.success("Your selections have been submitted successfully!")
+                    st.write(pd.DataFrame(selected_players))  # Optionally display selected players in a table
+
+
+if __name__ == '__main__':
+    user_input()
