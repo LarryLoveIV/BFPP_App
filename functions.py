bbdfba70f220bb64c6a6736f8f45efdf749db6f7
Larry Love <lawrenceloveiv@gmail.com>
2024-12-12 20:49:28 -0500

    created space with markdown inserts utilized player name with team in parenthesis

--- functions.py
+++ functions.py
@@ -8,29 +8,6 @@ ws1 = sh1.worksheet('2024 Player Pool')
 
 df = pd.DataFrame.from_dict(ws1.get_all_records())
 
-        #
-        # if st.button("Submit"):
-        #     # # Save user selections to Google Sheets
-        #     # client = authenticate_google_sheets()
-        #     # sheet = client.open('Fantasy Football Draft Selections').sheet1
-        #
-        #     # Prepare data to be saved
-        #     data = {
-        #         'Name': user_name,
-        #         'QB': qb_selection,
-        #         'RB1': rb_selections[0] if len(rb_selections) > 0 else '',
-        #         'RB2': rb_selections[1] if len(rb_selections) > 1 else '',
-        #         'WR1': wr_selections[0] if len(wr_selections) > 0 else '',
-        #         'WR2': wr_selections[1] if len(wr_selections) > 1 else '',
-        #         'TE': te_selection,
-        #         'Flex': section_5_selection,
-        #     }
-        #
-        #     # # Append to Google Sheet
-        #     # sheet.append_row([data[key] for key in data])
-        #
-        #     st.success(f"Thank you, {user_name}! Your selections have been submitted.")
-
 # Create a dictionary for holding player selections
 selected_players = []
 
@@ -43,40 +20,82 @@ def user_input():
 
         # Create user input form for player selections
         with st.form(key='team_selection'):
+
+            st.markdown("""
+                  <style>
+                      .stMultiSelect [data-baseweb="select"] span{
+                          max-width: none !important;
+                          white-space: normal !important;
+                          overflow: visible !important;
+                          text-overflow: clip !important;
+                      }
+                  </style>
+                  """, unsafe_allow_html=True)
+
             # QB Selection
             qb_choices = df[df['Position'] == 'QB']
-            qb = st.multiselect('Select 1 QB:', qb_choices['Player'].tolist(), max_selections=1)
+            qb = st.multiselect('Select 1 QB:', qb_choices['Player_Team'].tolist(), max_selections=1)
             selected_players = [{'Player': player, 'Position': 'QB',
-                                 'Team': qb_choices[qb_choices['Player'] == player]['Team'].values[0]} for player in qb]
+                                 'Team': qb_choices[qb_choices['Player_Team'] == player]['Team'].values[0]} for player in qb]
+
+            st.markdown("")
+            st.markdown("")
+            st.markdown("")
+            st.markdown("")
+            st.markdown("")
 
             # RB Selections (limit to 2 players)
             rb_choices = df[df['Position'] == 'RB']
-            rbs = st.multiselect('Select 2 RBs:', rb_choices['Player'].tolist(), max_selections=2)
+            rbs = st.multiselect('Select 2 RBs:', rb_choices['Player_Team'].tolist(), max_selections=2)
             selected_players += [{'Player': player, 'Position': 'RB',
-                                  'Team': rb_choices[rb_choices['Player'] == player]['Team'].values[0]} for player in
+                                  'Team': rb_choices[rb_choices['Player_Team'] == player]['Team'].values[0]} for player in
                                  rbs]
+
+            st.markdown("")
+            st.markdown("")
+            st.markdown("")
+            st.markdown("")
+            st.markdown("")
 
             # WR Selections (limit to 2 players)
             wr_choices = df[df['Position'] == 'WR']
-            wrs = st.multiselect('Select 2 WRs:', wr_choices['Player'].tolist(), max_selections=2)
+            wrs = st.multiselect('Select 2 WRs:', wr_choices['Player_Team'].tolist(), max_selections=2)
             selected_players += [{'Player': player, 'Position': 'WR',
-                                  'Team': wr_choices[wr_choices['Player'] == player]['Team'].values[0]} for player in
+                                  'Team': wr_choices[wr_choices['Player_Team'] == player]['Team'].values[0]} for player in
                                  wrs]
+
+            st.markdown("")
+            st.markdown("")
+            st.markdown("")
+            st.markdown("")
+            st.markdown("")
 
             # TE Selection
             te_choices = df[df['Position'] == 'TE']
-            te = st.multiselect('Select 1 TE:', te_choices['Player'].tolist(), max_selections=1)
+            te = st.multiselect('Select 1 TE:', te_choices['Player_Team'].tolist(), max_selections=1)
             selected_players += [{'Player': player, 'Position': 'TE',
-                                  'Team': te_choices[te_choices['Player'] == player]['Team'].values[0]} for player in
+                                  'Team': te_choices[te_choices['Player_Team'] == player]['Team'].values[0]} for player in
                                  te]
+
+            st.markdown("")
+            st.markdown("")
+            st.markdown("")
+            st.markdown("")
+            st.markdown("")
 
             # Flexible Position Selection (RB/WR/TE)
             flex_choices = df[df['Position'].isin(['RB', 'WR', 'TE'])]
-            flex = st.multiselect('Select 1 additional player from RB/WR/TE:', flex_choices['Player'].tolist(),
+            flex = st.multiselect('Select 1 additional player from RB/WR/TE:', flex_choices['Player_Team'].tolist(),
                                   max_selections=1)
             selected_players += [
-                {'Player': player, 'Position': flex_choices[flex_choices['Player'] == player]['Position'].values[0],
-                 'Team': flex_choices[flex_choices['Player'] == player]['Team'].values[0]} for player in flex]
+                {'Player': player, 'Position': flex_choices[flex_choices['Player_Team'] == player]['Position'].values[0],
+                 'Team': flex_choices[flex_choices['Player_Team'] == player]['Team'].values[0]} for player in flex]
+
+            st.markdown("")
+            st.markdown("")
+            st.markdown("")
+            st.markdown("")
+            st.markdown("")
 
             # Submit button
             submit_button = st.form_submit_button("Submit Selection")
